f9ce5f39c94eec23943485b1300462bb7b78c834
Bevilax <matheusbevilacqua@hotmail.com>
2022-05-14 20:57:32 +0000

    addingInstagramDiagram

--- src/models.py
+++ src/models.py
@@ -17,8 +17,6 @@ class User(Base):
 
 class Address(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
@@ -84,4 +82,40 @@ class Favorite(Base):
     
 
 ## Draw from SQLAlchemy base
+
+# Here will start the instagram diagram as the repo for the instagram diagram exercise is not working properly
+
+class UserInstagram(Base):
+    __tablename__ = 'userInstagram'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Post (Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    poster_id = Column(Integer, ForeignKey('userInstagram.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('userInstagram.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media (Base):
+    __tablename__ = 'media'
+    id = Column (Integer, primary_key=True)
+    type = Column (String(250), nullable=False)
+    url = Column (String(250), nullable=False)
+    post_id = Column (Integer, ForeignKey('post.id'))
+
+class Follower (Base):
+    __tablename__ = 'follower'
+    id = Column (Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('userInstagram.id'))
+    user_to_id = Column(Integer, ForeignKey('userInstagram.id'))
+
 render_er(Base, 'diagram.png')
